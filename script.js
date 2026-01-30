985edd54d1ec3ec141deba464d8dec3210d770fd
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-30 21:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const sounds = [
+  'applause',
+  'boo',
+  'gasp',
+  'tada',
+  'victory',
+  'wrong'
+];
+
+const buttons = document.querySelectorAll('.btn');
+const stopBtn = document.querySelector('.stop');
+
+const audioElements = {};
+
+sounds.forEach(sound => {
+  const audio = document.createElement('audio');
+  audio.src = `sounds/${sound}.mp3`;
+  audioElements[sound] = audio;
+});
+
+// Play sound
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.innerText;
+    stopAllSounds();
+    audioElements[sound].play();
+  });
+});
+
+// Stop all sounds
+stopBtn.addEventListener('click', stopAllSounds);
+
+function stopAllSounds() {
+  sounds.forEach(sound => {
+    audioElements[sound].pause();
+    audioElements[sound].currentTime = 0;
+  });
+}
